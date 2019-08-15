90f746cab61c86a92fe6b6b6f820a7ab3cf82119
Isaac <isaacamunenwa@gmail.com>
2019-08-14 22:03:06 -1100

    First commit

--- organizer/tests.py
+++ organizer/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .views import StartupDetail
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+
+# Create your tests here.
+class TemplateTest(TestCase):
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest() 
+		response = StartupDetail(request)
+		expected_html = render_to_string('startup_detail.html')  
+		self.assertEqual(response.content.decode(), expected_html) 
